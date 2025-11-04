8e60542d960ea6592fa51add729759aa7326b633
jsvetter <juliussimonvetter@gmail.com>
2025-11-04 08:54:17 +0100

    add mackelab plotting

--- src/stuff/test.py
+++ src/stuff/test.py
@@ -1,4 +1,16 @@
+# %%
 import numpy as np
+import matplotlib.pyplot as plt
 
 print("This is a test file.")
 print(np.arange(10))
+
+# %%
+with plt.rc_context(fname="../../matplotlibrc"):
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.title("Sine Wave")
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.show()
